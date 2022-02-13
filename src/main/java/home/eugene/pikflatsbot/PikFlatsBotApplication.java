bf99685c40e3417eb0ecf55a67bef9d787687a97
Eugene Chirkov <eugene.chirkov.ufa@gmail.com>
2022-02-13 14:02:17 +0300

    spring boot generate

---
+++ src/main/java/home/eugene/pikflatsbot/PikFlatsBotApplication.java
@@ -0,0 +1,13 @@
+package home.eugene.pikflatsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikFlatsBotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PikFlatsBotApplication.class, args);
+  }
+
+}
---
+++ src/test/java/home/eugene/pikflatsbot/PikFlatsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package home.eugene.pikflatsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikFlatsBotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
